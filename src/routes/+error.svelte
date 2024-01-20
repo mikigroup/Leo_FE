65bae1f4b7eafce14345781e761c1a5ec894bc91
mikigroup <mikigroup@gmail.com>
2024-01-20 12:31:22 +0100

    1

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,7 +5,8 @@
 <h1>Chyba 404</h1>
   <div class="text-center">        
     <h2 class="mt-6 text-base leading-7 text-gray-600">A jé, něco se stalo.</h2>
-    <div class="mt-10 flex items-center justify-center gap-x-6">
+    <div class="flex items-center justify-center mt-10 gap-x-6">
       <a href="/" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Zpět na domovskou stránku</a>      
     </div>
+    ¯\_(ツ)_/¯
   </div>
