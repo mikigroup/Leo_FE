9cea98462c809a835270b753b1546a965018bb26
mikigroup <mikigroup@gmail.com>
2024-01-20 16:36:08 +0100

    1

--- src/lib/component/Header.svelte
+++ src/lib/component/Header.svelte
@@ -1,30 +1,35 @@
 <script>
   import { page } from "$app/stores";
-  import { Icon, FaceSmile } from "svelte-hero-icons";  
-  import logo from "../logo_leo_fe.svg";  
+  /* import { Icon, FaceSmile } from "svelte-hero-icons"; */
+  import { ArrowLongRight } from "svelte-heros-v2";
+  import logo from "../logo_leo_fe.svg";
 </script>
-<header>  
-<nav class="">
-  <div class="flex flex-col items-center md:flex-row">
-    <div class="flex w-full">      
-      <div class="flex justify-end w-full md:justify-start">
-        <a href="/" class="flex mr-4">
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Malý LEO</span>
-          <img class="ml-5 rounded-full" src="{logo}" alt="Malý Leo" />          
-        </a>
-      </div>
-    </div>
-    <div class="flex flex-col items-center justify-center w-full border rounded-lg md:flex-row flex-1/2 md:border-none">
-      <div class="flex justify-center m-5 text-lg md:w-full">              
-          Vítej cizinče                
-      </div>
 
-      <div class="flex flex-row justify-center w-full p-2 m-5 border rounded-lg text-md xl:text-lg">
-        
+<header>
+  <nav class="">
+    <div class="flex flex-row items-center">
+      <div class="flex w-full">
+        <div class="flex md:justify-start">
+          <a href="/" class="flex logo">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap">Malý LEO</span>
+            <img class="ml-5 rounded-full" src={logo} alt="Malý Leo" />
+          </a>
+        </div>
       </div>
-      <div class="flex justify-end mb-5 md:w-full md:mb-0">
+      <div class="flex items-center justify-center">
+        <button class="flex items-center w-64 gap-5 p-4 m-2 transition-transform transform bg-black poptavka hover:text-black rounded-3xl hover:bg-slate-400 hover:scale-105">
+          <span>Objednat poptávku</span>
+          <span class="ml-2"><ArrowLongRight class="" /></span>
+        </button>
       </div>
     </div>
+  </nav>
+<section>
+<div class="headerBanner">
+  <div class="headerBanner01">
+    <h1>Malý Leo - 1st.</h1>
+    <p>Získejte konkurenční výhodu s naším eshopovým řešením, které se škáluje od drobných řemeslníků až po menší podniky. Postavený na nejnovějších technologiích, náš systém vyniká jednoduchým designem a flexibilitou.</p>
   </div>
-</nav>
+</div>
+</section>
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,4 @@
-<div class="headerBanner">
-  <div class="headerBanner01">
-<h1>Malý Leo - 1st.</h1>
-<p>Získejte konkurenční výhodu s naším eshopovým řešením, které se škáluje od drobných řemeslníků až po menší podniky. Postavený na nejnovějších technologiích, náš systém vyniká jednoduchým designem a flexibilitou.</p>
+<div>
+TEXT
+  
 </div>
-</div>
-
-
