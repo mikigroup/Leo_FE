e9f1d87f3e74797e9747082e6c6e2411c4194e35
mikigroup <mikigroup@gmail.com>
2024-02-01 15:57:28 +0100

    1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,8 @@
   import "../style.css";
   import Header from "../lib/component/Header.svelte";
   import Footer from "../lib/component/Footer.svelte";
-  import 'animate.css';
-  import 'wow.js';
+  /* import 'animate.css'; */
+  /* import 'wow.js'; */
  </script>
 <Header />
 <slot />
