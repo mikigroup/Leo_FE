ab367dc9620fb35c46540915d930c78f3255c9e9
mikigroup <mikigroup@gmail.com>
2024-02-08 15:17:25 +0100

    implementace Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,10 @@
   import Footer from "../lib/component/Footer.svelte";
   import Banner from "../lib/component/gdpr/Banner.svelte";
   import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
   injectSpeedInsights();
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Header />
