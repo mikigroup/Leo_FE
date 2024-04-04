5c62792a86b207ef27e500baa80f45995f98523e
mikigroup <39709130+mikigroup@users.noreply.github.com>
2024-04-04 14:39:20 +0200

    předělán form na validaci zod bez postupnych kroku

--- src/lib/component/Form.svelte
+++ src/lib/component/Form.svelte
@@ -1,85 +1,113 @@
 <script>
   import { page } from "$app/stores";
-  import { superForm } from "sveltekit-superforms";    
+  import { superForm } from "sveltekit-superforms";
   import SuperDebug from "sveltekit-superforms";
   import { schema } from "./schemaForm";
+  import { zod } from "sveltekit-superforms/adapters";
   export let data;
 
-  const { form, errors, message, enhance, delayed, options } = superForm(data.form, {
+  const { form, errors, message, enhance, delayed } = superForm(data.form, {
     resetForm: true,
-    validators: schema,
-    dataType: "json",
+     validators: zod(schema),
+    /* dataType: "json", */
+ /*    async onSubmit({ cancel }) {
+      const result = await validateForm({ update: true });
+      if (result.valid) {
+      } else {
+      }
+    },
+
+    async onUpdated({ form }) {}, */
   });
 
-  $: options.validators = schema;
+/* $: options.validators = schema; */
 </script>
 
-<form class="w-full max-w-lg p-10 my-10" method="POST" use:enhance>
+<form class="w-full max-w-lg p-10 my-10 border rounded-xl" method="POST" use:enhance>
   <div class="flex flex-wrap mb-6 -mx-3">
+    {#if $message}
+      <div class="status" class:error={$page.status >= 400} class:success={$page.status == 200}>
+        {$message}
+      </div>
+    {/if}
 
-         {#if $message}
-            <div class="status" class:error={$page.status >= 400} class:success={$page.status == 200}>
-              {$message}
-            </div>
-          {/if}
-
-        {#if form.error}
-          <div role="alert" class="alert alert-error">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 stroke-current shrink-0" fill="none" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <span>Chyba! Požadavek se nepovedl.</span>
-          </div>
-        {/if}
-        
+    {#if form.error}
+      <div role="alert" class="alert alert-error">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 stroke-current shrink-0" fill="none" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        <span>Chyba! Požadavek se nepovedl.</span>
+      </div>
+    {/if}
 
     <div class="w-full px-3 mb-6 md:w-1/2 md:mb-0">
-      <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="first_name">
-        Jméno
-      </label>
-      <input class="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="first_name" type="text" placeholder="Jana" aria-invalid={$errors.first_name ? "true" : undefined} bind:value={$form.first_name}>
+      <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="first_name"> Jméno </label>
+      <input
+        class="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+        id="first_name"
+        type="text"
+        placeholder="Jana"
+        aria-invalid={$errors.first_name ? "true" : undefined}
+        bind:value={$form.first_name} />
       <!-- <p class="text-xs italic text-red-500">Vyplň prosím</p> -->
     </div>
     <div class="w-full px-3 md:w-1/2">
-      <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="last_name">
-        Příjmení
-      </label>
-      <input class="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="last_name" type="text" placeholder="Werichová" aria-invalid={$errors.last_name ? "true" : undefined} bind:value={$form.last_name}>
+      <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="last_name"> Příjmení </label>
+      <input
+        class="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+        id="last_name"
+        type="text"
+        placeholder="Werichová"
+        aria-invalid={$errors.last_name ? "true" : undefined}
+        bind:value={$form.last_name} />
     </div>
-  </div> 
+  </div>
   <div class="flex flex-wrap mb-6 -mx-3">
     <div class="w-full px-3 mb-6 md:w-1/2 md:mb-0">
-      <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="email">
-        Email
-      </label>
-      <input class="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="text" placeholder="jana.werichova@" aria-invalid={$errors.email ? "true" : undefined} bind:value={$form.email}>
+      <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="email"> Email </label>
+      <input
+        class="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+        id="email"
+        type="text"
+        placeholder="jana.werichova@"
+        aria-invalid={$errors.email ? "true" : undefined}
+        bind:value={$form.email} />
     </div>
-    <div class="w-full px-3 md:w-1/2"> 
-      <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="telephone">
-        Telefon 
-      </label>
-      <input class="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="telephone" type="text" placeholder="777111222" aria-invalid={$errors.telephone ? "true" : undefined} bind:value={$form.telephone}>
+    <div class="w-full px-3 md:w-1/2">
+      <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="telephone"> Telefon </label>
+      <input
+        class="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+        id="telephone"
+        type="text"
+        placeholder="777111222"
+        aria-invalid={$errors.telephone ? "true" : undefined}
+        bind:value={$form.telephone} />
     </div>
   </div>
   <div class="flex flex-wrap mb-6 -mx-3">
     <div class="w-full px-3">
-      <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="text">
-        Zpráva
-      </label>
-      <textarea class="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" cols="43" rows="10" id="text" type="text" placeholder="Mám zájem o..." aria-invalid={$errors.text ? "true" : undefined} bind:value={$form.text}></textarea>
+      <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="text"> Zpráva </label>
+      <textarea
+        class="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+        cols="43"
+        rows="10"
+        id="text"
+        type="text"
+        placeholder="Mám zájem o..."
+        aria-invalid={$errors.text ? "true" : undefined}
+        bind:value={$form.text}></textarea>
     </div>
 
     <button class="p-2 mt-4 border btn btn-primary rounded-xl hover:bg-slate-100">Konec</button>
-          {#if $delayed}<div class="loading loading-dots loading-xs"></div>{/if}
+    {#if $delayed}<div class="loading loading-dots loading-xs"></div>{/if}
 
-      <div class="py-4 text-sm text-red-700 rounded-lg" role="alert">
-          {#if $errors.first_name}<p>{$errors.first_name}</p>{/if}
-          {#if $errors.last_name}<p>{$errors.last_name}</p>{/if}
-          {#if $errors.email}<p>{$errors.email}</p>{/if}
-          {#if $errors.telephone}<p>{$errors.telephone}</p>{/if}
-          {#if $errors.text}<p>{$errors.text}</p>{/if}
-        </div>
+    <div class="py-4 text-sm text-red-700 rounded-lg" role="alert">
+      {#if $errors.first_name}<p>{$errors.first_name}</p>{/if}
+      {#if $errors.last_name}<p>{$errors.last_name}</p>{/if}
+      {#if $errors.email}<p>{$errors.email}</p>{/if}
+      {#if $errors.telephone}<p>{$errors.telephone}</p>{/if}
+      {#if $errors.text}<p>{$errors.text}</p>{/if}
+    </div>
   </div>
   <SuperDebug data={$form} />
 </form>
- 
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,7 +5,6 @@ import { fail } from "@sveltejs/kit";
 import { PUBLIC_seznamKey } from "$env/static/public";
 import nodemailer from "nodemailer";
 
-
 /** @type {import('./$types').Load} */
 export const load = async () => {  
   const form = await superValidate(zod(schema));
@@ -39,3 +38,27 @@ export const actions = {
     return message(form, "Formulář v pořádku odeslán!");
   },
 };
+
+/* import { zod } from "sveltekit-superforms/adapters";
+import { message, superValidate } from "sveltekit-superforms/server";
+import { schema } from "../lib/component/schemaForm";
+import { fail } from "@sveltejs/kit";
+
+export const load = async () => {
+  const form = await superValidate(zod(schema));
+  return { form };
+};
+
+export const actions = {
+  default: async ({ request }) => {
+    const formData = await request.formData();
+    console.log(formData);
+
+    const form = await superValidate(formData, zod(schema));
+    console.log(form);
+
+    if (!form.valid) return fail(400, { form });
+
+    return message(form, 'Posted OK!');
+  }
+}; */
