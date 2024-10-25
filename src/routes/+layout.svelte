fecd0445fa3acf26efa95b9487db87027ceface0
mikigroup <mikigroup@gmail.com>
2024-10-25 19:12:00 +0200

    1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,22 @@
   import Header from "../lib/component/Header.svelte";
   import Footer from "../lib/component/Footer.svelte";
   import Banner from "../lib/component/gdpr/Banner.svelte";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+
+	function preventWidows(selector) {
+		const elements = document.querySelectorAll(selector);
+
+		elements.forEach(element => {
+			element.innerHTML = element.innerHTML.replace(/\b(a|i|o|u|s|k|v|z) /gi, '$1&nbsp;');
+		});
+	}
+
+	preventWidows('p');
+	}
+)
+
 </script>
 
 <Header />
