2f3f7a1066f0da64995dd91f10bec2d716fc6e1b
mikigroup <39709130+mikigroup@users.noreply.github.com>
2024-11-14 14:40:48 +0100

    1

--- src/lib/component/Header.svelte
+++ src/lib/component/Header.svelte
@@ -14,39 +14,39 @@
 				<h1 class="text-2xl 2xl:text-3xl font-bold lg:mb-6">
 					Malý Leo - webové řešení pro váš byznys
 				</h1>
-				<p class="mb-5 lg:mb-10 text-xl 2xl:text-2xl leading-7">
+				<p class="mb-11 mt-9 text-xl 2xl:text-2xl leading-7">
 					Potřebujete rychlý, moderní a spolehlivý web?
 					<br />
 					Nabízíme tři hlavní benefity.
 				</p>
-				<div class="p-10">
+				<div class="">
 					<ul
 						class="list-inside flex flex-col lg:flex-row gap-5 lg:gap-5 leading-7 text-sm">
-						<li class="pl-2">
+						<li class="basis-1/3">
 							<span
-								class="argumenty inline-block text-white rounded-full px-4 py-2 mr-2 text-md 2xl:text-lg">
-								Výkon
+								class="argumenty1 inline-block px-4 py-2 mr-2 text-md 2xl:text-lg">
+								VÝKON
 							</span>
 							<p class="lg:mt-4 text-lg 2xl:text-xl text-center">
 								Optimalizované webové aplikace s rychlým načítáním a plynulým
 								chodem
 							</p>
 						</li>
-						<li class="pl-2">
+						<li class="basis-1/3">
 							<span
-								class="argumenty1 inline-block bg-yellow-500 text-white rounded-full px-4 py-2 mr-2 text-md 2xl:text-lg">
-								Spolehlivost
+								class="argumenty2 inline-block px-4 py-2 mr-2 text-md 2xl:text-lg">
+								SPOLEHLIVOST
 							</span>
-							<p class="lg:mt-4 text-lg 2xl:text-xl">
+							<p class="lg:mt-4 text-lg 2xl:text-xl a2 text-center">
 								Bezpečný provoz s pravidelnými zálohami dat
 							</p>
 						</li>
-						<li class="pl-2">
+						<li class="basis-1/3">
 							<span
-								class="argumenty2 inline-block bg-green-500 text-white rounded-full px-4 py-2 mr-2 text-md 2xl:text-lg">
-								Design
+								class="argumenty3 inline-block px-4 py-2 mr-2 text-md 2xl:text-lg">
+								DESIGN
 							</span>
-							<p class="lg:mt-4 text-lg 2xl:text-xl">
+							<p class="lg:mt-4 text-lg 2xl:text-xl a3 text-center">
 								Přizpůsobitelný Vašim potřebám
 							</p>
 						</li>
@@ -61,15 +61,33 @@
 	@use "$lib/styles/_mixins.scss" as mixins;
 	@use "$lib/styles/_variables.scss" as vars;
 
-	.argumenty {
-		background-color: #ad9e00;
+  .a2 {
+		color: vars.$color2;
+	};
+
+	.a3 {
+    color: vars.$color3;
+  };
+
+  .argumenty1, .argumenty2, .argumenty3 {
+		width: 100%;
+		padding: 1.5rem;
+		font-size: 1.2rem;
+    text-align: center;
+		color: white;
+		border: 1px solid white;
+    border-radius: 1.8rem;
 	}
-	.argumenty1 {
-		background-color: #709900;
-	}
+
+  .argumenty1 {
+		background-color: vars.$color1;
+	};
 	.argumenty2 {
-		background-color: #008349;
-	}
+    background-color: vars.$color2;
+	};
+	.argumenty3 {
+    background-color: vars.$color3;
+	};
 
 	.headerBanner01 {
 		background-color: rgba(255, 255, 255, 0.85);
