946dd653fbaa21408b6a537d7b4a9b09bba6b8db
mikigroup <39709130+mikigroup@users.noreply.github.com>
2024-12-13 10:27:13 +0100

    1

--- src/lib/component/Form.svelte
+++ src/lib/component/Form.svelte
@@ -16,11 +16,22 @@
 
 	async function verifyRecaptcha() {
 		try {
+			if (typeof grecaptcha === 'undefined') {
+				console.error('reCAPTCHA není načtena');
+				return null;
+			}
+
 			recaptchaToken = await new Promise((resolve) => {
 				grecaptcha.ready(() => {
-					grecaptcha.execute().then((token) => {
-						resolve(token);
-					});
+					grecaptcha
+						.execute(RECAPTCHA_KEY)
+						.then((token) => {
+							resolve(token);
+						})
+						.catch((error) => {
+							console.error('Chyba při získávání tokenu:', error);
+							resolve(null);
+						});
 				});
 			});
 			return recaptchaToken;
